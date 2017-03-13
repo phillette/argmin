65295862711be843e5ea0005446f9961682c42fb
Hanshan <tim.niven.public@gmail.com>
2017-03-13 15:58:56 +0800

    Progress, but still far away.

--- align1.py
+++ align1.py
@@ -4,44 +4,79 @@ import tensorflow as tf
 HIDDEN_SIZE = 300
 BATCH_SIZE = 3
 NUM_ITERS = 50000
-EMBEDDING_DIM = 300
+WORD_EMBED_DIM = 300
+SENT_EMBED_DIM = 100
+LONGEST_SENTENCE = 50
 
 
 def data():
     with tf.name_scope('data'):
         # batch size x no. time steps x embedding dimension
-        premises = tf.placeholder(tf.float32, [BATCH_SIZE, None, EMBEDDING_DIM], name='premises')
-        hypotheses = tf.placeholder(tf.float32, [BATCH_SIZE, None, EMBEDDING_DIM], name='hypotheses')
+        premises = tf.placeholder(tf.float64, [BATCH_SIZE, None, WORD_EMBED_DIM], name='premises')
+        hypotheses = tf.placeholder(tf.float64, [BATCH_SIZE, None, WORD_EMBED_DIM], name='hypotheses')
         return premises, hypotheses
 
 
-def create_sentence_rnn(sentences, name):
-    with tf.name_scope(name + '_rnn'):
-        cell = tf.nn.rnn_cell.GRUCell(HIDDEN_SIZE)
-        initial_state = tf.placeholder_with_default(cell.zero_state(BATCH_SIZE,
-                                                                    tf.float32),
-                                                    shape=[None, HIDDEN_SIZE],
-                                                    name=name + '_initial_state')
-        max_sentence_length = tf.reduce_sum(tf.reduce_max(tf.sign(sentences), 2), 1)
-        output, output_state = tf.nn.dynamic_rnn(cell,             # cell
-                                                 sentences,        # inputs
-                                                 max_sentence_length,  # sequence length
-                                                 initial_state)    # initial state
-        return output, initial_state, output_state
+def create_rnn(sentences, name):
+    with tf.variable_scope(name + '_rnn_scope'):
+        with tf.name_scope(name + '_rnn'):
+            cell = tf.contrib.rnn.GRUCell(HIDDEN_SIZE)
+            initial_state = tf.placeholder_with_default(cell.zero_state(BATCH_SIZE,
+                                                                        tf.float64),
+                                                        shape=[None, HIDDEN_SIZE],
+                                                        name=name + '_initial_state')
+            length = tf.reduce_sum(tf.reduce_max(tf.sign(sentences), 2), 1)
+            #length = tf.multiply(tf.ones([BATCH_SIZE]), 50)
+            output, output_state = tf.nn.dynamic_rnn(cell,              # cell
+                                                     sentences,         # inputs
+                                                     length,            # sequence length
+                                                     initial_state)     # initial state
+            return output, initial_state, output_state
 
 
-"""
-How do I know the shape of the outputs?  In putting the lengths in the tf.nn.dynamic_rnn, am I
-telling it how many output units?  I am relying on this being the case in the below.
-I could and should find a way to isolate and test this.  Should be doable with a test
-function and a feed_dict.  Confirm the behaviour to be expected before building out too far!
-"""
+def create_rnns(premises, hypotheses):
+    premises_output, _, _ = create_rnn(premises, 'premises')
+    hypotheses_output, _, _ = create_rnn(hypotheses, 'hypotheses')
+    return premises_output, hypotheses_output
+
+
+def encode_sentences(sentence_rnn_output, name):
+    with tf.variable_scope(name + '_encode_scope'):
+        W_s1 = tf.Variable(tf.random_uniform([WORD_EMBED_DIM, SENT_EMBED_DIM],
+                                             -1.0, 1.0, dtype=tf.float64),
+                           name='W_s1')
+        W_s2 = tf.Variable(tf.random_uniform([1, LONGEST_SENTENCE],
+                                             -1.0, 1.0, dtype=tf.float64),
+                           name='W_s2')
+        encode1 = tf.map_fn(lambda h: tf.tanh(tf.matmul(h, W_s1)),
+                            sentence_rnn_output,
+                            name='encode1')
+        encoding = tf.map_fn(lambda h: tf.tanh(tf.matmul(W_s2, h)),
+                             encode1,
+                             name=name + '_encoding')
+        return encoding
 
 
 def align(premise_output, hypothesis_output):
     similarity = None  # define the similarity function
     alignments = None
     return 4
+
+
+def classify(encoded_premises, encoded_hypotheses):
+    # BATCH_SIZE x 1 x 200
+    concatenated_sentences = tf.concat([encoded_premises, encoded_hypotheses],
+                                       axis=2,
+                                       name='concatenated_sentences')
+    concatenated_sentences_with_bias = tf.concat([tf.ones([BATCH_SIZE, 1, 1],
+                                                          dtype=tf.float64),
+                                                  concatenated_sentences],
+                                                 axis=2)
+    W_h1 = tf.Variable(tf.random_uniform([BATCH_SIZE, 2 * SENT_EMBED_DIM + 1]))
+    hidden_layer_z = tf.placeholder(tf.float64,
+                                    [BATCH_SIZE, 1, 200],
+                                    name='hidden_layer_z')
+
 
 
 def train(batch_gen):
--- align_tests.py
+++ align_tests.py
@@ -6,17 +6,29 @@ import numpy as np
 
 class Align1Tests(unittest.TestCase):
     def setUp(self):
-        pass
+        self.premises, self.hypotheses = data()
+        self.premises_input = np.random.randn(3, 50, 300)
+        self.hypotheses_input = np.random.rand(3, 50, 300)
+        self.feed_dict = {self.premises: self.premises_input,
+                          self.hypotheses: self.hypotheses_input}
+        self.premises_output, self.hypotheses_output = create_rnns(self.premises,
+                                                                   self.hypotheses)
+        self.encoded_premises = encode_sentences(self.premises_output,
+                                                 'encoded_premises')
+        self.encoded_hypotheses = encode_sentences(self.hypotheses_output,
+                                                   'encoded_hypotheses')
+        self.concatenation = classify(self.encoded_premises,
+                                      self.encoded_hypotheses)
+        self.sess = tf.Session()
 
-    def test_sentence_rnn_output(self):
-        premises, hypotheses = data()
-        sentences = np.random.randn(3, 8, 300)  # at the moment it's like this - pre-padding out per batch
-        output, initial_state, output_state = create_sentence_rnn(premises, 'premises')
-        with tf.Session() as sess:
-            sess.run(tf.global_variables_initializer())
-            result = sess.run(output, {premises: sentences})
-            print(type(result))
-            print(result.shape)
+    def test_rnn_output(self):
+        p = self.sess.run(self.premises_output, self.feed_dict)
+        h = self.sess.run(self.hypotheses_output, self.feed_dict)
+        print(p.shape)
+        print(h.shape)
+
+    def tearDown(self):
+        self.sess.close()
 
 
 if __name__ == '__main__':
