2cefffb0283f626b8761a941bc54c97b75b1602e
Tim Niven <tim.niven.public@outlook.com>
2017-03-21 21:18:06 +0800

    Fixed a bug in accuracy function

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ from prediction import accuracy
 if __name__ == '__main__':
     model = BiRNN(learning_rate=1e-3)
     train(model, 'train', 2, load_ckpt=False)
-    accuracy(model, 'dev', load_ckpt=False)
-    accuracy(model, 'test', load_ckpt=False)
+    accuracy(model, 'dev')
+    accuracy(model, 'test')
