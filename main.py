ad5f03393deb910d4f178c5a9872cb666f77b761
Hanshan <tim.niven.public@gmail.com>
2017-03-24 21:20:45 +0800

    Transfer checkpointing.

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from prediction import accuracy
 
 
 if __name__ == '__main__':
-    model = BiRNN(learning_rate=1e-12)  # originally 1e-3 with good results
-    #train(model, 'carstens', 10, load_ckpt=True, transfer=True)
+    model = BiRNN(learning_rate=1e-14)  # originally 1e-3 with good results
+    train(model, 'carstens', 10, load_ckpt=True, transfer=True)
     accuracy(model, 'carstens')
