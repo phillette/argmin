70781c5e2e22d1ad1fa77a4ea6b961142346260c
Hanshan <tim.niven.public@gmail.com>
2017-03-31 10:42:20 +0800

    main for ubuntu.

--- main.py
+++ main.py
@@ -8,16 +8,16 @@ import tensorflow as tf
 
 
 if __name__ == '__main__':
-    model = LSTMEncoder(learning_rate=1e-3,
-                        p_keep_rnn=1.0,
-                        p_keep_ff=1.0,
-                        p_keep_input=1.0,
-                        grad_clip_norm=5.0)
+    model = BiRNN(learning_rate=3e-3,
+                  p_keep_rnn=0.5,
+                  p_keep_ff=0.5,
+                  grad_clip_norm=5.0)
     db = 'snli'
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        train(model, db, 'dev', 5, sess, load_ckpt=False, save_ckpt=True, transfer=False)
+        train(model, db, 'train', 10, sess, load_ckpt=False, save_ckpt=True, transfer=False)
         accuracy(model, db, 'dev', sess)
+        accuracy(model, db, 'test', sess)
 
 # 54.9306144334 (dev for sure, maybe also test)
 # 73% accuracy on full set of Carstens - looks like it can go higher from the end of training pattern/tendency
