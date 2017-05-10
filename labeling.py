767c67ea307ac34fb5773b10ae80b1be8f1ad614
Tim Niven <tim.niven.public@outlook.com>
2017-05-10 19:44:07 +0800

    Added node labels to labeling.

--- labeling.py
+++ labeling.py
@@ -2,9 +2,10 @@ LABEL_TO_ENCODING = {
     'neutral': 0,
     'entailment': 1,
     'contradiction': 2,
-    'n': 0,
-    's': 1,
-    'a': 2
+    'null': 0,  # node
+    'n': 0,  # carstens
+    's': 1,  # carstens
+    'a': 2  # carstens
 }
 ENCODING_TO_LABEL = {
     0: 'neutral',
