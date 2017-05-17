c07257de822790eee38a2dd82e093840be4d2aa5
Tim Niven <tim.niven.public@outlook.com>
2017-05-17 15:11:09 +0800

    Continue to debug batching.

--- batching.py
+++ batching.py
@@ -98,8 +98,22 @@ class TransferBatch:
         self.labels = np.vstack(labels)
 
 
+def add_third_dimension(sentence_matrices):
+    """Adds third 'batch' dimension to batch data.
+
+    Args:
+      sentence_matrices: a list of sentence matrices.
+    Raises:
+      EmptyListError: if the list of sentence matrices is empty.
+    """
+    if len(sentence_matrices) == 0:
+        raise errors.EmptyListError('sentence_matrices')
+    return np.concatenate(list(M[np.newaxis, ...]
+                               for M in sentence_matrices),
+                          axis=0)
+
+
 def get_batch_gen(db, collection, batch_size=None):
-    # if a batch_size is not selected, default to the preferred
     if not batch_size:
         batch_size = get_batch_size(db, collection)
     gen = RandomGenerator(db, collection, buffer_size=batch_size * 2)
@@ -120,7 +134,11 @@ def get_batch_gen(db, collection, batch_size=None):
                 premises.append(premise)
                 hypotheses.append(hypothesis)
                 labels.append(label)
-                pad_length = update_pad_length(pad_length, premise, hypothesis)
+                pad_length = update_pad_length(pad_length,
+                                               premise,
+                                               hypothesis)
+            else:
+                gen._raise_exception()
         premises = pad_sentences(premises, pad_length)
         hypotheses = pad_sentences(hypotheses, pad_length)
         batch = Batch(ids, premises, hypotheses, labels)
@@ -157,6 +175,44 @@ def get_batch_size(db, collection):
     return min(np.floor(collection_size / 10), 32)
 
 
+def num_iters(db, collection, batch_size=None, subset_size=None):
+    if not batch_size:
+        batch_size = PREFERRED_BATCH_SIZES[db][collection]
+    if subset_size:
+        return int(np.ceil(subset_size / batch_size))
+    collection_size = stats.COLLECTION_SIZE[db][collection]
+    return int(np.ceil(collection_size / batch_size))
+
+
+def pad_sentences(sentences, pad_length):
+    """Pad out a list of sentences with zero vectors.
+
+    Args:
+      sentences: list of sentence matrices.
+      pad_length: integer length to pad out to.
+    """
+    return list(pad_sentence(premise, pad_length)
+                for premise in sentences)
+
+
+def pad_sentence(sentence, pad_length):
+    """Pad out a sentence with zero vectors.
+
+    Args:
+      sentence: sentence matrix to pad out.
+      pad_length: integer length to pad out to.
+    """
+    original_length = sentence.shape[0]
+    word_embed_dim = sentence.shape[1]
+    if original_length < pad_length:
+        sentence = np.concatenate(
+            [sentence,
+             np.zeros((pad_length - original_length,
+                       word_embed_dim))],
+            axis=0)
+    return sentence
+
+
 def update_pad_length(pad_length, premise, hypothesis):
     premise_length = premise.shape[0]
     if pad_length < premise_length:
@@ -165,43 +221,3 @@ def update_pad_length(pad_length, premise, hypothesis):
     if pad_length < hypothesis_length:
         pad_length = hypothesis_length
     return pad_length
-
-
-def pad_sentences(sentences, pad_length):
-    return list(pad_sentence(premise, pad_length)
-                for premise in sentences)
-
-
-def pad_sentence(sentence, pad_length):
-    original_length = sentence.shape[0]
-    word_embed_dim = sentence.shape[1]
-    if original_length < pad_length:
-        sentence = np.concatenate([sentence,
-                                   np.zeros((pad_length - original_length,
-                                             word_embed_dim))],
-                                  axis=0)
-    return sentence
-
-
-def add_third_dimension(sentence_matrices):
-    """Adds third 'batch' dimension to batch data.
-
-    Args:
-      sentence_matrices: a list of sentence matrices.
-    Raises:
-      EmptyListError: if the list of sentence matrices is empty.
-    """
-    if len(sentence_matrices) == 0:
-        raise errors.EmptyListError('sentence_matrices')
-    return np.concatenate(list(M[np.newaxis, ...]
-                               for M in sentence_matrices),
-                          axis=0)
-
-
-def num_iters(db, collection, batch_size=None, subset_size=None):
-    if not batch_size:
-        batch_size = PREFERRED_BATCH_SIZES[db][collection]
-    if subset_size:
-        return int(np.ceil(subset_size / batch_size))
-    collection_size = stats.COLLECTION_SIZE[db][collection]
-    return int(np.ceil(collection_size / batch_size))
