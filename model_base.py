First attempt to fix dropout and also applying gradients to biases.

model_base.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import decorators
 import util
+import dropout
 
 
 def config(embed_size=300,
@@ -12,7 +13,9 @@ def config(embed_size=300,
            p_keep_rnn=1.0,
            p_keep_input=1.0,
            representation_learning_rate=5e-4,
-           classification_learning_rate=5e-4):
+           classification_learning_rate=5e-4,
+           linear_logits_output=3,
+           linear_learning_rate=0.01):
     return {
         'embed_size': embed_size,
         'learning_rate': learning_rate,
@@ -23,55 +26,37 @@ def config(embed_size=300,
         'p_keep_rnn': p_keep_rnn,
         'p_keep_input': p_keep_input,
         'representation_learning_rate': representation_learning_rate,
-        'classification_learning_rate': classification_learning_rate
+        'classification_learning_rate': classification_learning_rate,
+        'linear_logits_output': linear_logits_output,
+        'linear_learning_rate': linear_learning_rate
     }
 
 
 def fully_connected_with_dropout(inputs,
                                  num_outputs,
                                  activation_fn,
-                                 p_keep,
-                                 scale_factor):
+                                 dropout_config,
+                                 dropout_key='ff'):
     fully_connected = tf.contrib.layers.fully_connected(
         inputs=inputs,
-        num_outputs=num_outputs,  # need to dynamically set this...
+        num_outputs=int(num_outputs / dropout_config.raw[dropout_key]),
         activation_fn=activation_fn)
-    dropped_out = tf.nn.dropout(fully_connected, p_keep)
-    factored = tf.multiply(dropped_out, scale_factor)
-    return factored
-
-
-def keep_probability(p_keep, training_flag):
-    """Op to get dropout keep probability.
-
-    Args:
-      p_keep: real number probability of keeping a neuron.
-      training_flag: if in training 1.0 if not 0.0.
-
-    Returns:
-      Tensor (scalar) of keep probability usable for dropout op.
-    """
-    return tf.subtract(
-        1.0,
-        tf.multiply(
-            training_flag,
-            tf.subtract(1.0, p_keep)))
-
-
-def post_dropout_factor(p_keep, training_flag):
-    return tf.subtract(
-        1.,
-        tf.multiply(
-            tf.subtract(1., training_flag),
-            tf.subtract(1., p_keep)))
+    dropped_out = tf.nn.dropout(
+        x=fully_connected,
+        keep_prob=dropout_config.ops[dropout_key])
+    return dropped_out
 
 
 class Model:
     def __init__(self, config):
-        self.training_flag = tf.placeholder(tf.float32, [])
-        self.p_keep = {}
-        self.scale_factor = {}
+        self.training_flag = tf.placeholder(
+            dtype=tf.float64,
+            shape=[],
+            name='training_flag')
         self.config = config
+        self.dropout_config = dropout.DropoutConfig(
+            config=self.config,
+            training_flag=self.training_flag)
         # I reckon I could clean these up with a function
         self.embed_size = config['embed_size']
         self.learning_rate = config['learning_rate']
@@ -89,9 +74,6 @@ class Model:
         self._training_variables()
         self._training_placeholders()
         self._training_ops()
-        self.in_training = False  # dunno about this one
-        # new dropout stuff
-        self._init_dropout()
         self.premises
         self.hypotheses
         self.Y
@@ -111,6 +93,10 @@ class Model:
         return tf.shape(self.premises)[1]
 
     @decorators.define_scope
+    def classifier_input(self):
+        raise NotImplementedError()
+
+    @decorators.define_scope
     def confidences(self):
         return tf.reduce_max(self.logits, axis=1)
 
@@ -124,6 +110,26 @@ class Model:
             tf.float64,
             [None, None, self.embed_size],
             name='hypotheses')
+
+    @decorators.define_scope
+    def linear_logits(self):
+        return tf.contrib.layers.fully_connected(
+            inputs=self.classifier_input,
+            num_outputs=self.config['linear_logits_output'],
+            activation_fn=tf.sigmoid)
+
+    @decorators.define_scope
+    def linear_loss(self):
+        cross_entropy = tf.reduce_sum(
+            tf.nn.sparse_softmax_cross_entropy_with_logits(
+                labels=tf.argmax(self.Y, axis=1),
+                logits=self.linear_logits,
+                name='softmax_cross_entropy'))
+        penalty_term = tf.multiply(
+            tf.cast(self.lamda, tf.float64),
+            sum([tf.nn.l2_loss(w) for w in self._all_weights()]),
+            name='penalty_term')
+        return tf.add(cross_entropy, penalty_term, name='loss')
 
     @decorators.define_scope
     def loss(self):
@@ -141,8 +147,9 @@ class Model:
     @decorators.define_scope
     def optimize(self):
         optimizer = tf.train.AdamOptimizer(self.learning_rate)
-        grads_and_vars = optimizer.compute_gradients(self.loss,
-                                                     self._all_weights())
+        grads_and_vars = optimizer.compute_gradients(
+            self.loss,
+            self._all_weights() + self._all_biases())
         if self.grad_clip_norm > 0.0:
             grads_and_vars = util.clip_gradients(grads_and_vars,
                                                  norm=self.grad_clip_norm)
@@ -167,6 +174,16 @@ class Model:
         grads_and_vars = optimizer.compute_gradients(
             self.loss,
             weights_to_optimize)
+        if self.grad_clip_norm > 0.0:
+            grads_and_vars = util.clip_gradients(grads_and_vars,
+                                                 norm=self.grad_clip_norm)
+        return optimizer.apply_gradients(grads_and_vars)
+
+    @decorators.define_scope
+    def optimize_linear_logits(self):
+        optimizer = tf.train.AdamOptimizer(self.config['linear_learning_rate'])
+        grads_and_vars = optimizer.compute_gradients(self.linear_loss,
+                                                     self._all_weights())  # HERE'S A BIG QUESTION: WHY NOT BIASES HERE?
         if self.grad_clip_norm > 0.0:
             grads_and_vars = util.clip_gradients(grads_and_vars,
                                                  norm=self.grad_clip_norm)
@@ -229,6 +246,11 @@ class Model:
             [None, 3],
             name='y')
 
+    def _all_biases(self):
+        return [v for
+                v in tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
+                if v.name.endswith('biases:0')]
+
     def _all_weights(self):
         return [v for
                 v in tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
@@ -240,27 +262,18 @@ class Model:
                 if w.name.startswith('logits')]
 
     def _init_backend(self):
+        self.classifier_input
         self.logits
         self.loss
         self.optimize
+        self.linear_logits
+        self.linear_loss
+        self.optimize_linear_logits
         self.predicted_labels
         self.correct_predictions
         self.accuracy
         self.confidences
         self.summary
-
-    def _init_dropout(self):
-        for key in [k for k
-                    in self.config.keys()
-                    if k.startswith('p_keep')]:
-            self.p_keep[key.split('p_keep')[1]] = \
-                keep_probability(
-                    p_keep=self.config[key],
-                    training_flag=self.training_flag)
-            self.scale_factor[key.split('p_keep')[1]] = \
-                post_dropout_factor(
-                    p_keep=self.config[key],
-                    training_flag=self.training_flag)
 
     def _init_transfer_optimization(self):
         self.optimize_representation

rnn_encoders.py
@@ -5,20 +5,21 @@ import util
 
 
 def bi_rnn(sentences, hidden_size, scope,
-           p_keep=0.8, cell=tf.contrib.rnn.BasicLSTMCell):
+           dropout_config, dropout_key='rnn',
+           cell=tf.contrib.rnn.BasicLSTMCell):
     sequence_length = util.length(sentences)
     forward_cell = cell(
-        hidden_size,
+        int(hidden_size / dropout_config.raw[dropout_key]),
         forget_bias=1.0)
     forward_cell = tf.contrib.rnn.DropoutWrapper(
         cell=forward_cell,
-        output_keep_prob=p_keep)
+        output_keep_prob=dropout_config.ops[dropout_key])
     backward_cell = cell(
-        hidden_size,
+        int(hidden_size / dropout_config.raw[dropout_key]),
         forget_bias=1.0)
     backward_cell = tf.contrib.rnn.DropoutWrapper(
         cell=backward_cell,
-        output_keep_prob=p_keep)
+        output_keep_prob=dropout_config.ops[dropout_key])
     output, output_states = tf.nn.bidirectional_dynamic_rnn(
         cell_fw=forward_cell,
         cell_bw=backward_cell,
@@ -70,29 +71,42 @@ class Encoder(model_base.Model):
         raise NotImplementedError()
 
     @decorators.define_scope
-    def logits(self):
-        self.ff_input = tf.concat(
+    def classifier_input(self):
+        input = tf.concat(
             [self.premises_encoding,
-             tf.abs(tf.subtract(self.premises_encoding,
-                                self.hypotheses_encoding)),
-             self.hypotheses_encoding],
+             self.hypotheses_encoding,
+             tf.abs(tf.subtract(
+                 self.premises_encoding,
+                 self.hypotheses_encoding)),
+             tf.multiply(
+                 self.premises_encoding,
+                 self.hypotheses_encoding)],
             axis=1,
             name='concatenated_encodings')
-        self.ff_input = tf.nn.dropout(self.ff_input,
-                                      self.p_keep_input)
+        dropped_input = tf.nn.dropout(
+            x=input,
+            keep_prob=self.dropout_config.ops['input'])
+        return dropped_input
+
+    @decorators.define_scope
+    def logits(self):
         h1 = tf.contrib.layers.fully_connected(
-            inputs=self.ff_input,
-            num_outputs=self.hidden_size,
+            inputs=self.classifier_input,
+            # looking at this now I think this should be in another config...
+            num_outputs=int(self.hidden_size / self.dropout_config.raw['ff']),
             activation_fn=tf.tanh)
-        self.h1 = tf.nn.dropout(h1, self.p_keep)
+        h1_dropped = tf.nn.dropout(
+            x=h1,
+            keep_prob=self.dropout_config.ops['ff'])
         h2 = tf.contrib.layers.fully_connected(
-            inputs=self.h1,
-            num_outputs=self.hidden_size,
+            inputs=h1_dropped,
+            num_outputs=int(self.hidden_size / self.dropout_config.raw['ff']),
             activation_fn=tf.tanh)
-        if self.in_training:
-            h2 = tf.nn.dropout(h2, self.p_keep)
+        h2_dropped = tf.nn.dropout(
+            x=h2,
+            keep_prob=self.dropout_config.ops['ff'])
         _logits = tf.contrib.layers.fully_connected(
-            inputs=h2,
+            inputs=h2_dropped,
             num_outputs=3,
             activation_fn=None)
         return _logits
@@ -135,10 +149,10 @@ class BiLSTMEncoder(Encoder):
     def premises_encoding(self):
         # [batch_size, timesteps, rnn_size]
         _, output_states = bi_rnn(
-            self.premises,
-            self.embed_size,
-            'premise_bi_rnn',
-            self.p_keep)
+            sentences=self.premises,
+            hidden_size=self.embed_size,
+            scope='premise_bi_rnn',
+            dropout_config=self.dropout_config)
         return tf.concat([v.h for v in output_states],
                          axis=1)
 
@@ -146,10 +160,10 @@ class BiLSTMEncoder(Encoder):
     def hypotheses_encoding(self):
         # [batch_size, timesteps, rnn_size]
         _, output_states = bi_rnn(
-            self.hypotheses,
-            self.embed_size,
-            'hypothesis_bi_rnn',
-            self.p_keep)
+            sentences=self.hypotheses,
+            hidden_size=self.embed_size,
+            scope='hypothesis_bi_rnn',
+            dropout_config=self.dropout_config)
         return tf.concat([v.h for v in output_states],
                          axis=1)
 